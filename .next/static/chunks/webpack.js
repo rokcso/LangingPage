3e0332639ff53d99a41676a18ebea4c323cfb575
waringhu <waringhu@163.com>
2023-09-16 22:14:04 +0800

    add body margin

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "91c642d2b16129e7"; }
+/******/ 		__webpack_require__.h = function() { return "5d137f0ae2181152"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/profile.js
+++ components/profile.js
@@ -3,14 +3,13 @@ import styles from "./profile.module.css";
 export default function Profile() {
   return (
     <>
-      <header className={styles.header}>
+      <div className={styles.header}>
       <img src="/assets/rokcso-studio-compressed.png" height="120" alt="rokcso Studio"></img>
-        <h2>:oh</h2>
+        <h4>Hi there 👋</h4>
         <h1>
-          This is <span>rokcso</span>, we build for ourselves first, and you
-          second.
+          This is <span>rokcso</span> Studio, we build for ourselves first, and you second.
         </h1>
-      </header>
+      </div>
     </>
   );
 }
