2861bedb4a02e19c42375bf4efde627b785ca9c7
waringhu <waringhu@163.com>
2023-11-14 15:50:21 +0800

    update profile

--- components/profile.js
+++ components/profile.js
@@ -9,9 +9,10 @@ export default function Profile() {
           height="120"
           alt="rokcso Studio"
         ></img>
-        <h4>Hi there 👋</h4>
+        <h4>Hi there. 👋</h4>
         <h1>
-          This is <span>rokcso</span>. I build for myself first.
+          This is <span>rokcso</span>. I know a little about development and
+          design. I build for myself first.
         </h1>
       </div>
     </>
